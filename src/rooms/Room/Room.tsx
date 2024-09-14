900c4edb52bc540ddb52a8bd1ba6bcd3f8de093d
Gargant <gargant0373@gmail.com>
2024-09-14 14:25:14 +0200

    more shit for rooms

--- src/rooms/Room/Room.tsx
+++ src/rooms/Room/Room.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import "./Room.css";
 
-interface RoomProps {
+export interface RoomProps {
     title: string;
     images: string[];
     description: string;
@@ -12,15 +12,33 @@ interface RoomProps {
     // private bathroom: true, DA CLAU?
 }
 
-function Room() {
+function Room(props: RoomProps) {
     const [open, setOpen] = useState(false);
+
+    const getPath = (ix: number) => {
+        return "images/" + props.images[ix];
+    }
 
     const toggle = () => {
         setOpen(!open);
     }
 
     return <>
-
+        <div className="room">
+            <img className="present" src={getPath(0)} alt={props.title} />
+            <div className="strip">{props.title}</div>
+            <div className="expand">
+                <button onClick={toggle}>+</button>
+                <span>VIEW ROOM DETAILS</span>
+                {open && <div className="details">
+                    <div className="description">
+                        <h1>{props.title}</h1>
+                        <p>{props.description}</p>
+                    </div>
+                </div>
+                }
+            </div>
+        </div>
     </>
 }
 
--- src/rooms/Rooms.tsx
+++ src/rooms/Rooms.tsx
@@ -1,10 +1,46 @@
+import Footer from "../misc/Footer/Footer";
 import Header from "../misc/Header/Header";
+import Room, { RoomProps } from "./Room/Room";
 import "./Rooms.css";
+
+const rooms: RoomProps[] = [
+    {
+        title: "Budget Room",
+        images: ["room1_0.jpg", "room1_1.jpg", "room1_2.jpg"],
+        description: "A budget room, pefect for a short stay. The room has a double bed and a private bathroom with a nice view of the garden.",
+        price: 220,
+        bathtub: false,
+        shower: true,
+        balcony: false
+    },
+    {
+        title: "Standard Room",
+        images: ["room2_0.jpg", "room2_1.jpg", "room2_2.jpg"],
+        description: "A standard room, perfect for a couple. The room has a double bed, a private bathroom with a bathtub.",
+        price: 300,
+        bathtub: true,
+        shower: true,
+        balcony: false
+    },
+    {
+        title: "Balcony Room",
+        images: ["room3_0.jpg", "room3_1.jpg", "room3_2.jpg", "room3_3.jpg"],
+        description: "A room with a balcony, perfect for a couple. The room has a double bed, a private bathroom with a shower.",
+        price: 350,
+        bathtub: true,
+        shower: true,
+        balcony: true
+    }
+]
 
 function Rooms() {
     return <>
         <section className="rooms">
             <Header image={"rooms.jpg"} selected={2} />
+            {rooms.map((room) => {
+                return <Room {...room} />
+            })}
+            <Footer />
         </section>
     </>
 }
